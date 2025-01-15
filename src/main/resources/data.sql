843a0935c7a1f634146df03df1a7187c1274ba77
Sounak <sounaksaha333@gmail.com>
2025-01-16 01:24:11 +0530

    0.1: creating table thorugh hibernate orm

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into jpa_practice.`product table` (title_x, price, quantity, sku)
+values
+('Parle Biscuit', 12.3, 2, 'parle243'),
+('Pepsi', 10.5, 5, 'pepsi395'),
+('TV', 2000.24, 1, 'tv999'),
+('Bluetooth Speaker', 799.99, 4, 'speaker374');
